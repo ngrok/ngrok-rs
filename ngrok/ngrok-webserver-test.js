f8134c4ff015dadf4ca56b0c1ac30361c388f129
bobzilladev <bob@ngrok.com>
2023-01-03 15:15:56 -0500

    simplify ngrok-webserver-test.js

--- ngrok/ngrok-webserver-test.js
+++ ngrok/ngrok-webserver-test.js
@@ -2,28 +2,12 @@ var UNIX_SOCKET = "/tmp/http.socket";
 const fs = require('fs');
 fs.unlinkSync(UNIX_SOCKET);
 
-// Import the Node.js http module
+// make webserver
 var http = require('http'); 
-  
-// req is the request object which is
-// coming from the client side
-// res is the response object which is going
-// to client as response from the server
-  
-// Create a server object
 http.createServer(function (req, res) {
-  
-// 200 is the status code which means
-// All OK and the second argument is
-// the object of response header.
-res.writeHead(200, {'Content-Type': 'text/html'}); 
-  
-    // Write a response to the client
-    res.write('Congrats you have a created an ngrok web server');
-  
-    // End the response
-    res.end();
-  
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('Congrats you have a created an ngrok web server');
+  res.end();
 })
 // .listen(8081); // Server object listens on port 8081
 //console.log('Node.js web server at port 8081 is running..');
