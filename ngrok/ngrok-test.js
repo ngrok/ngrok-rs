cc6c24d99ed55c71ed638ca2b22d98d061a2e2c8
bobzilladev <bob@ngrok.com>
2023-01-03 16:38:23 -0500

    remove ngrok-test.js

--- ngrok/ngrok-test.js
+++
@@ -1,9 +0,0 @@
-// Import the ngrok module
-var ngrok = require('.'); 
-ngrok.session().then((result) => {
-  result.startTunnel().then((r2) => {
-    console.log("got r2: " + r2)
-    console.log("got r2: " + r2.getId())
-  })
-})
-
